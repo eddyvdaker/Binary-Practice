5877ac7cfcbb689672d0ca6408f2ddb2e203fd86
Eddy van den Aker <eddy.v.d.aker@gmail.com>
2019-09-05 15:17:36 +0200

    made both cli and gui version startable

--- start.py
+++ start.py
@@ -1,5 +1,13 @@
+import argparse
+
 from binary_practice import start_cli, start_gui
 
 if __name__ == '__main__':
-    # start_cli()
-    start_gui()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--cli', action='store_true', 
+        help='Open with CLI')
+    args = parser.parse_args()
+    if args.cli:
+        start_cli()
+    else:
+        start_gui()
